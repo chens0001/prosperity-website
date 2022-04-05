ddf17f5c04756c7487b349a79bbb9d92bcb471e5
mac <mac@macdeMacBook-Air.local>
2022-04-05 09:07:25 +0800

    deploy

---
+++ precache-manifest.cf7c889d84aa43684d880e73567d2ce1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d79dd074c71eecfcdd3",
+    "url": "/prosperity-website/css/app.ed5dc547.css"
+  },
+  {
+    "revision": "6bdcecc15af84f72e7f0c74928e7cb1b",
+    "url": "/prosperity-website/index.html"
+  },
+  {
+    "revision": "8d79dd074c71eecfcdd3",
+    "url": "/prosperity-website/js/app.d3c4fcbf.js"
+  },
+  {
+    "revision": "c68de8e0e32738bbfd58",
+    "url": "/prosperity-website/js/chunk-vendors.50d2175c.js"
+  },
+  {
+    "revision": "f98480460e6fa5c99a35238e16446e4b",
+    "url": "/prosperity-website/manifest.json"
+  },
+  {
+    "revision": "996b8a5849046b054cb49237cde2adaa",
+    "url": "/prosperity-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/prosperity-website/robots.txt"
+  }
+]);
